b6d99ad5ae8be684105cf30740e0e69b03840522
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-25 22:31:07 -0700

    created models folder and routed to it from server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ console.log("hellow")
 
 const express = require("express");
 const logger = require("morgan");
-const path = require("path");
+// const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 // const db = require("./models");
-
+const Workout = require('./models/Workout')
 const app = express();
 
 app.use(logger("dev"));
@@ -21,9 +21,11 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
-var apiRoutes = require (path.join(__dirname, "routes/api.js"));
-app.use("/api", apiRoutes);
+// var apiRoutes = require (path.join(__dirname, "routes/api.js"));
+// app.use("/api", apiRoutes);
 
+require('./routes/api')(app);
+require('./routes/html')(app);
 
 
 
